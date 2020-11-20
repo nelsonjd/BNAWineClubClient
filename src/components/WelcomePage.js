05a07f5bedc837dea32478a220d1a38790d02645
Jordan D. Nelson <jordan.nelson@metova.com>
2020-11-20 16:04:42 -0600

    add day of week

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -18,7 +18,7 @@ class WelcomePage extends Component {
         William Overton<br />
         Aaron Villareal<br />
         & George Wilson,</p>
-        <p>cordially invite you to a formal evening of food and wine on December 3, 2020. Please select your ticket type below to view more details:</p>
+        <p>cordially invite you to a formal evening of food and wine on Thursday, December 3, 2020. Please select your ticket type below to view more details:</p>
         <button onClick={this.props.handleRegClick}>Reguar Ticket</button>
         <button onClick={this.props.handleVipClick}>VIP Ticket</button>
       </div>

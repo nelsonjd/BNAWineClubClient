31df14a01f352b8219eb812751352c384fef95e0
Jordan D. Nelson <jordan.nelson@metova.com>
2020-11-20 11:43:45 -0600

    update welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -2,14 +2,28 @@ import React, { Component } from "react";
 import "../WelcomePage.css";
 
 class WelcomePage extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <div className="welcome-page">
-      <h1>Welcome to Wine Club Formal</h1>
-      <p>The event will be held on Thursday, December 3, 2020.</p>
-      <p>Please select your ticket type below</p>
-      <button>Reguar Ticket</button>
-      <button>VIP Ticket</button>
-    </div>;
+    return (
+      <div className="welcome-page">
+        <h1>Welcome to Wine Club Formal</h1>
+        <p>Messrs.:<br />
+        Andrew Castillo<br />
+        Wilson Laine<br />
+        Jordan Nelson<br />
+        William Overton<br />
+        Aaron Villareal<br />
+        George Wilson
+        </p>
+        <p>cordially invite you to a formal evening of food and wine on December 3, 2020. Please select your ticket type below to view more details:</p>
+        <button onClick={this.props.handleRegClick}>Reguar Ticket</button>
+        <button>VIP Ticket</button>
+      </div>
+    );
   }
 }
 
